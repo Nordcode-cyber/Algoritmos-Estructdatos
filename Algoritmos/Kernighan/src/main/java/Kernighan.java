2c7b8bfb202377cb97e47bc76be288c7a798fb5c
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2026-04-06 20:35:29 -0300

    files

--- Algoritmos/Kernighan/src/main/java/Kernighan.java
+++ Algoritmos/Kernighan/src/main/java/Kernighan.java
@@ -14,7 +14,7 @@ public class Kernighan {
     return count;
     }//fin metodo countSetBits(int n)
    public static void main(String[] args) {
-       int n = 536870911;
+       int n = 16;
        System.out.println("el numero entero: " + n + " " + " : en binario es: " + Integer.toBinaryString(n));
     }
     }
