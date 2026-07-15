c9c8841df2cd766ca83021d219ec00872ecf8373
Norcode-cyber <Nordcode-cyber@users.noreply.github.com>
2026-07-14 23:08:38 -0300

    tree routing

--- Estructura-de-datos/Arboles/Enorden/src/main/java/Appenorden.java
+++ Estructura-de-datos/Arboles/Enorden/src/main/java/Appenorden.java
@@ -20,17 +20,28 @@ public class Appenorden {
     }
     public static void main(String[] args) {
           // Create binary tree
-        //       1
-        //      /  \
-        //    2     3
-        //   / \     \
-        //  4   5     6
+        //         1
+        //      /     \
+        //    2         3
+        //   /  \     /   \
+        //  4    5    6     7
+        // /\    /\   /\    /\
+        //8 9  10 11 12 13 14 15
          Nodo06 raiz = new Nodo06(1);
         raiz.izq = new Nodo06(2);
         raiz.der = new Nodo06(3);
         raiz.izq.izq = new Nodo06(4);
         raiz.izq.der = new Nodo06(5);
-        raiz.der.der = new Nodo06(6);
+        raiz.der.izq = new Nodo06(6);
+        raiz.der.der = new Nodo06(7);
+        raiz.izq.izq.izq = new Nodo06(8);
+        raiz.izq.izq.der = new Nodo06(9);
+        raiz.izq.der.izq = new Nodo06(10);
+        raiz.izq.der.der = new Nodo06(11);
+        raiz.der.izq.izq = new Nodo06(12);
+        raiz.der.izq.der = new Nodo06(13);
+        raiz.der.der.izq = new Nodo06(14);
+        raiz.der.der.der = new Nodo06(15);
         ArrayList<Integer> res = new ArrayList<>();
         Enorden(raiz, res);
         for(int node : res)
--- Estructura-de-datos/Arboles/Postorden/src/main/java/Apppostorden.java
+++ Estructura-de-datos/Arboles/Postorden/src/main/java/Apppostorden.java
@@ -14,11 +14,13 @@ public class Apppostorden {
     }
     public static void main(String[] args) {
         //representar arbol
-        //        1
-        //       /  \
-        //      2    3
-        //     / \  / \
-        //    4  5 6   7
+        //            1
+        //       /        \
+        //      2           3
+        //     /  \      /    \
+        //    4    5     6     7
+        //   / \  / \   / \   / \
+        //  8  9 10 11 12 13 14 15
         Nodo10 raiz = new Nodo10(1);
         raiz.izq = new Nodo10(2);
         raiz.der = new Nodo10(3);
@@ -26,14 +28,21 @@ public class Apppostorden {
         raiz.izq.der = new Nodo10(5);
         raiz.der.izq = new Nodo10(6);
         raiz.der.der = new Nodo10(7);
+        raiz.izq.izq.izq = new Nodo10(8);
+        raiz.izq.izq.der = new Nodo10(9);
+        raiz.izq.der.izq = new Nodo10(10);
+        raiz.izq.der.der = new Nodo10(11);
+        raiz.der.izq.izq = new Nodo10(12);
+        raiz.der.izq.der = new Nodo10(13);
+        raiz.der.der.izq = new Nodo10(14);
+        raiz.der.der.der = new Nodo10(15);
+
 
         ArrayList<Integer> result = new ArrayList<>();
         postOrden(raiz, result);
+        System.out.println("recorrido PostOrden del Arbol: ");
         for (int val: result)
             System.out.print(val + " ");
-
-
-        
     }
     
 }
